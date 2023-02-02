1c603a91cc8616888830b6a89251e87be3884f3a
saskiabntg16 <123155307+saskiabntg16@users.noreply.github.com>
2023-02-02 10:17:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle #to load a saved model
+import pickle
+import streamlit as st
 
 pickle_in = open('model_uas.pkl', 'rb')
 nb = pickle.load(pickle_in)
+
+def welcome():
+    return 'Selamat Datang'
 
 def prediction(age, sex, bmi, children, smoker):
 
@@ -12,39 +15,23 @@ def prediction(age, sex, bmi, children, smoker):
     print(prediction)
     return prediction
 
-app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
-
-if app_mode=='Home':
-    st.write('Nim : 2019230039')
-    st.write('Nama : Esa Meytha Shamirah')
+def main():
+    st.title("Aplikasi Prediksi Asuransi dengan Algoritma Linier Regression")
+    st.markdown('Oleh : Putri Dita Pratiwi (2019230001) | UAS Datamining')
     st.write('\n')
-    st.title('Pembayaran Premi Asuransi :') 
-    st.write('\n')
-    st.write('\n')
-    st.image('insurance.jpg')
-    st.title("Aplikasi Prediksi Pembayaran Premi Asuransis Dengan Algoritma Regresi Linier")
-    st.markdown('Dataset :')
-    data=pd.read_csv('insurance1.csv')
-    st.write(data.head())
-
-elif app_mode == 'Prediction':
-    st.write('\n')
-    st.write('Nim : 2019230039')
-    st.write('Nama : Esa Meytha Shamirah')
-    st.write('\n')
-    st.title('Silakan, isi form berikut ini :')
+    st.markdown('Silakan isi form berikut terlebih dahulu :')
     
     st.write('\n')
-    age = st.number_input("Age", 0)
-    sex = st.number_input("Sex (Male = 0, Female = 1)", 0)
-    bmi = st.number_input("BMI", 0)
-    children = st.number_input("Children", 0)
-    smoker = st.number_input("Smoker (Yes = 1, No = 0)", 0)
+    age = st.number_input("age", 0)
+    sex = st.number_input("sex", 0)
+    bmi = st.number_input("bmi", 0)
+    children = st.number_input("children", 0)
+    smoker = st.number_input("smoker", 0)
     result =""
     
-    if st.button("SUBMIT"):
+    if st.button("PREDIKSI"):
         result = prediction(age, sex, bmi, children, smoker)
-    st.success('Hasil Prediksi Dengan Algoritma Regresi Linier = {}'.format(result))
+    st.success('Charges {}'.format(result))
     
 if __name__=='__main__':
     main()
