664cb2f8487552156ebcc8732e8ae7f90a9a8f75
saskiabntg16 <123155307+saskiabntg16@users.noreply.github.com>
2023-02-02 10:13:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
 import pickle #to load a saved model
 
 pickle_in = open('model_uas.pkl', 'rb')
@@ -16,14 +15,13 @@ def prediction(age, sex, bmi, children, smoker):
 app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
 
 if app_mode=='Home':
-    st.write('Nama : Saskia Bintang Maharani')
-    st.write('NIM : 2019230047')
+    st.write('Nim : 2019230039')
+    st.write('Nama : Esa Meytha Shamirah')
     st.write('\n')
     st.title('Pembayaran Premi Asuransi :') 
     st.write('\n')
     st.write('\n')
-    img = Image.open ('insurance.jpg')
-    st.image(img, use_column_width=False)
+    st.image('insurance.jpg')
     st.title("Aplikasi Prediksi Pembayaran Premi Asuransis Dengan Algoritma Regresi Linier")
     st.markdown('Dataset :')
     data=pd.read_csv('insurance1.csv')
@@ -31,22 +29,21 @@ if app_mode=='Home':
 
 elif app_mode == 'Prediction':
     st.write('\n')
-    st.write('Nama : Saskia Bintang Maharani')
-    st.write('Nim : 2019230047')
+    st.write('Nim : 2019230039')
+    st.write('Nama : Esa Meytha Shamirah')
     st.write('\n')
-    st.title('Isi Data Dibawah Ini :')
+    st.title('Silakan, isi form berikut ini :')
     
     st.write('\n')
-            age = st.number_input("Age", 0)
+    age = st.number_input("Age", 0)
     sex = st.number_input("Sex (Male = 0, Female = 1)", 0)
     bmi = st.number_input("BMI", 0)
     children = st.number_input("Children", 0)
     smoker = st.number_input("Smoker (Yes = 1, No = 0)", 0)
     result =""
     
-
-    
-    if st.button("Pedict"):
+    if st.button("SUBMIT"):
         result = prediction(age, sex, bmi, children, smoker)
+    st.success('Hasil Prediksi Dengan Algoritma Regresi Linier = {}'.format(result))
 
     st.success('Hasil Prediksi Dengan Algoritma Regresi Linier = {}'.format(result))
